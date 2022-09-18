5f678b85ff4341a0cab299598ab2bef150e005dd
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-09-17 23:32:05 -0700

    Added: Calculate Empwage Calculation

--- src/main/java/Employeewage.java
+++ src/main/java/Employeewage.java
@@ -1,15 +1,18 @@
 import  java .util.*;
 public class Employeewage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employeewage Computation");
+        System.out.println("Welcome to Employeewage Calculation");
         int Fulltime = 1;
+        int perhour = 20;
+        int emphrs = 0,empwage = 0;
         Random random = new Random();
         int empcheck = random.nextInt(2);
                 if(empcheck==Fulltime){
-                    System.out.println("Present");
+                  emphrs = 8;
                 }else{
-                    System.out.println("Absent");
+                    emphrs = 0;
                 }
-
+                empwage = emphrs*perhour;
+        System.out.println("calculate empwage :"+empwage);
     }
 }
