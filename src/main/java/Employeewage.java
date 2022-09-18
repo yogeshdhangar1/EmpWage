67b16127e5bbb344f3455ba0c7938f87818fb44c
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-09-17 23:18:07 -0700

    Added: To Check EmpAttendence

--- src/main/java/Employeewage.java
+++ src/main/java/Employeewage.java
@@ -1,5 +1,15 @@
+import  java .util.*;
 public class Employeewage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employeewage Computation");
+        int Fulltime = 1;
+        Random random = new Random();
+        int empcheck = random.nextInt(2);
+                if(empcheck==Fulltime){
+                    System.out.println("Present");
+                }else{
+                    System.out.println("Absent");
+                }
+
     }
 }
