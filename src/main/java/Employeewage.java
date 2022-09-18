df8f3b3c815da46043f13e5c3405d42713d3a68b
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-09-17 23:45:01 -0700

    Added: Calculate Parttime_Empwage Calculation

--- src/main/java/Employeewage.java
+++ src/main/java/Employeewage.java
@@ -2,17 +2,20 @@ import  java .util.*;
 public class Employeewage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employeewage Calculation");
+        int Parttime = 0;
         int Fulltime = 1;
         int perhour = 20;
-        int emphrs = 0,empwage = 0;
+        int emphrs =0,empwage=0;
         Random random = new Random();
         int empcheck = random.nextInt(2);
-                if(empcheck==Fulltime){
-                  emphrs = 8;
-                }else{
-                    emphrs = 0;
-                }
-                empwage = emphrs*perhour;
-        System.out.println("calculate empwage :"+empwage);
+        if(empcheck==Parttime){
+            emphrs = 4;
+        }else if(empcheck==Fulltime){
+            emphrs = 8;
+        }else{
+            emphrs = 0;
+        }
+        empwage = emphrs*perhour;
+        System.out.println("Empwage Calculate:"+empwage);
     }
 }
